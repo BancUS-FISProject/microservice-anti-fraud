db100a386bb737f9a6e2230bc8f597d658795450
angelmr-01 <angelmrod01@gmail.com>
2025-11-28 17:28:52 +0100

    fix(ci): fix errors related to GitHub Actions pipeline

--- src/anti-fraud/dto/check-transaction.dto.ts
+++ src/anti-fraud/dto/check-transaction.dto.ts
@@ -1,19 +1,29 @@
 import { ApiProperty } from '@nestjs/swagger';
 
-
 export class CheckTransactionDto {
   @ApiProperty({ example: 30, description: 'Unique transaction ID' })
   transactionId: number;
 
-  @ApiProperty({ example: 1, description: 'User ID performing the transaction' })
+  @ApiProperty({
+    example: 1,
+    description: 'User ID performing the transaction',
+  })
   userId: number;
 
-  @ApiProperty({ example: 1500.50, description: 'Transaction amount' })
+  @ApiProperty({ example: 1500.5, description: 'Transaction amount' })
   amount: number;
 
-  @ApiProperty({ example: 'ES912100...', description: 'Source account (IBAN or ID)', required: false })
+  @ApiProperty({
+    example: 'ES912100...',
+    description: 'Source account (IBAN or ID)',
+    required: false,
+  })
   origin: string;
 
-  @ApiProperty({ example: 'ES910045...', description: 'Destination account (IBAN or ID)', required: false })
-  destination:string;
+  @ApiProperty({
+    example: 'ES910045...',
+    description: 'Destination account (IBAN or ID)',
+    required: false,
+  })
+  destination: string;
 }
