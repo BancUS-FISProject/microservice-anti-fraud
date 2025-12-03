0e58e1e80ec55092e8add94b4db32977e8601511
angelmr-01 <angelmrod01@gmail.com>
2025-12-03 14:03:56 +0100

    feat(microservices): correct endpoints and add RabbitMQ integration

--- src/anti-fraud/anti-fraud.controller.ts
+++ src/anti-fraud/anti-fraud.controller.ts
@@ -1,22 +1,46 @@
+<<<<<<< Updated upstream
 import { Controller, Get, Post, Body, Param, HttpCode, HttpStatus, ForbiddenException, ParseIntPipe } from '@nestjs/common';
 import { AntiFraudService } from './anti-fraud.service';
 import { CheckTransactionDto } from './dto/check-transaction.dto';
 import { CreateFraudReportDto } from './dto/create-fraud-report.dto';
 import { ApiOperation, ApiResponse, ApiTags, ApiParam, ApiBody } from '@nestjs/swagger';
+=======
+import { 
+  Controller, 
+  Get, 
+  Post, 
+  Body, 
+  Param, 
+  HttpCode, 
+  HttpStatus, 
+  ForbiddenException, 
+  ParseIntPipe 
+} from '@nestjs/common';
+import { EventPattern, Payload } from '@nestjs/microservices';
+import { AntiFraudService } from './anti-fraud.service';
+import { CheckTransactionDto } from './dto/check-transaction.dto';
+import { 
+  ApiOperation, 
+  ApiResponse, 
+  ApiTags, 
+  ApiParam, 
+  ApiBody 
+} from '@nestjs/swagger';
+>>>>>>> Stashed changes
 
 @ApiTags('Anti-Fraud')
 @Controller('v1') 
 export class AntiFraudController {
   constructor(private readonly antiFraudService: AntiFraudService) {}
 
-  // Endpoint Interno: Validar transacción antes de aprobarla. POST /v1/fraud-alerts/check
   @Post('fraud-alerts/check')
-  @ApiOperation({ summary: 'Validate transaction risk' })
+  @ApiOperation({ summary: 'Check if a transaction is risky.' })
   @ApiBody({
     type: CheckTransactionDto,
     examples: {
       safeCase: {
         summary: 'Example: Safe Transaction',
+<<<<<<< Updated upstream
         description: 'Low amount transaction between known accounts.',
         value: {
           transactionId: 30,
@@ -35,21 +59,43 @@ export class AntiFraudController {
           amount: 5000,
           origin: 'ES-ACCOUNT-666',
           destination: 'KY-OFFSHORE-999'
+=======
+        value: { 
+          transactionId: 1001, userId: 50, amount: 500, origin: 'ES-111', destination: 'ES-222' 
+        }
+      },
+      fraudCase: {
+        summary: 'Example: Risk Transaction',
+        value: { 
+          transactionId: 9999, userId: 666, amount: 2500, origin: 'ES-666', destination: 'KY-OFFSHORE-999' 
+>>>>>>> Stashed changes
         }
       }
     }
   })
   @ApiResponse({ status: 200, description: 'Transaction approved.' })
+<<<<<<< Updated upstream
   @ApiResponse({ status: 403, description: 'Transaction rejected due to high risk.' })
+=======
+  @ApiResponse({ status: 403, description: 'Transaction rejected and account blocked.' })
+>>>>>>> Stashed changes
   @HttpCode(HttpStatus.OK)
   async checkTransaction(@Body() data: CheckTransactionDto) {
     const isRisky = await this.antiFraudService.checkTransactionRisk(data);
     if (isRisky) {
+<<<<<<< Updated upstream
       throw new ForbiddenException({ message: 'Transaction rejected', code: 'HIGH_RISK' });
+=======
+      throw new ForbiddenException({ 
+        message: 'Transaction rejected', 
+        code: 'HIGH_RISK' 
+      });
+>>>>>>> Stashed changes
     }
-    return { status: 'APPROVED', message: 'Transaction seems safe' };
+    return { status: 'APPROVED', message: 'Transaction accepted' };
   }
 
+<<<<<<< Updated upstream
 
   // Obtener alertas de posible fraude. GET /v1/users/{userId}/fraud-alerts
 @ Get('users/:userId/fraud-alerts')
@@ -83,3 +129,20 @@ export class AntiFraudController {
     return await this.antiFraudService.reportFraud(movementId, body.userId, body.reason);
   }
 }
+=======
+  @EventPattern('transaction_created')
+  async handleTransactionCreated(@Payload() data: CheckTransactionDto) {
+    await this.antiFraudService.checkTransactionHistory(data);
+  }
+
+
+
+  @Get('users/:userId/fraud-alerts')
+  @ApiOperation({ summary: 'Retrieve transaction history to analize possible past risk transactions' })
+  @ApiParam({ name: 'userId', example: 666, description: 'Target User ID (Numeric)' })
+  @ApiResponse({ status: 200, description: 'List of alerts retrieved successfully.' })
+  async getUserAlerts(@Param('userId', ParseIntPipe) userId: number) {
+    return this.antiFraudService.getAlertsForUser(userId);
+  }
+}
+>>>>>>> Stashed changes
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { Transport, MicroserviceOptions } from '@nestjs/microservices';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // 2. Conectar el Microservicio RabbitMQ. Esto hace que la app escuche en la cola 'antifraud_queue'
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.RMQ,
+    options: {
+      urls: [process.env.RABBITMQ_URL || 'amqp://localhost:5672'],
+      queue: 'antifraud_queue', // Nombre de la cola de este microservicio, donde se conectaran el resto de microservicios.
+      queueOptions: {
+        durable: false
+      },
+    },
+  });
+
 
   const config = new DocumentBuilder()
     .setTitle('Anti-Fraud Microservice')
@@ -17,7 +31,7 @@ async function bootstrap() {
   });
   SwaggerModule.setup('api', app, document); // La documentacion estara en /api
 
-
+  await app.startAllMicroservices();
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
